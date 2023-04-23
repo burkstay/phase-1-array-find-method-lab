33b1e4d08ba9c457371a644d98b76073eba75762
Taylor Burks <tayyy25@gmail.com>
2023-04-23 17:34:51 -0400

    array find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(function(element){
+        if(element.result ===`W`){
+            return element
+        }
+    })
+    if(win){
+        return win.year
+    }
+    else{return undefined}
+}
